4e43b4e588233d70b98e4b9149a246ee77130857
Amy Boudsady <aeimskei@gmail.com>
2018-03-21 16:38:52 -0700

    Amy's branch test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-
+// Amy's branch :) 
 
 
 if (process.env.NODE_ENV !== 'test') {
