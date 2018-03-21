86d12e0c503206d61bbf95ab40761521d2c69744
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-03-21 16:31:34 -0700

    shell of project created, here goes nothin'

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+
+
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Snacks are running on port ${port}!`)
+  })
+}
+module.exports = app
