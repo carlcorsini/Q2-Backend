8defe27da34976080b02e194ee8ed541c16d567a
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-04-05 21:07:06 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -30,10 +30,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      host: 'postgres://bcydhneacicnse:69ccce18d8df7df3f71e8dd3c25b77144db173d3cd3e78ae8d9c7602f687f8a3@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d5se2nbl8lp6me',
-      database: 'the_vibe'
-    },
+    connection: 'postgres://bcydhneacicnse:69ccce18d8df7df3f71e8dd3c25b77144db173d3cd3e78ae8d9c7602f687f8a3@ec2-54-221-192-231.compute-1.amazonaws.com:5432/d5se2nbl8lp6me',
     migrations: {
       directory: './db/migrations'
     },
